8eab4bfdd345ec1006d4b4338e8e1d76ee8bd8f3
akaporn-katip <akaporn.katip@gmail.com>
2024-09-30 14:57:57 +0700

    .

--- k6/script.js
+++ k6/script.js
@@ -1,66 +1,44 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+import http from "k6/http";
+import { sleep } from "k6";
+import { URL } from 'https://jslib.k6.io/url/1.0.0/index.js';
 
 export const options = {
-  stages: [
-    { duration: '10s', target: 10 }
-  ]
-  // A number specifying the number of VUs to run concurrently.
-  //vus: 40,
-  // A string specifying the total duration of the test run.
-  //duration: '60s',
-
-  // The following section contains configuration options for execution of this
-  // test script in Grafana Cloud.
-  //
-  // See https://grafana.com/docs/grafana-cloud/k6/get-started/run-cloud-tests-from-the-cli/
-  // to learn about authoring and running k6 test scripts in Grafana k6 Cloud.
-  //
-  // ext: {
-  //   loadimpact: {
-  //     // The ID of the project to which the test is assigned in the k6 Cloud UI.
-  //     // By default tests are executed in default project.
-  //     projectID: "",
-  //     // The name of the test in the k6 Cloud UI.
-  //     // Test runs with the same name will be grouped.
-  //     name: "script.js"
-  //   }
-  // },
-
-  // Uncomment this section to enable the use of Browser API in your tests.
-  //
-  // See https://grafana.com/docs/k6/latest/using-k6-browser/running-browser-tests/ to learn more
-  // about using Browser API in your test scripts.
-  //
-  // scenarios: {
-  //   // The scenario name appears in the result summary, tags, and so on.
-  //   // You can give the scenario any name, as long as each name in the script is unique.
-  //   ui: {
-  //     // Executor is a mandatory parameter for browser-based tests.
-  //     // Shared iterations in this case tells k6 to reuse VUs to execute iterations.
-  //     //
-  //     // See https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/ for other executor types.
-  //     executor: 'shared-iterations',
-  //     options: {
-  //       browser: {
-  //         // This is a mandatory parameter that instructs k6 to launch and
-  //         // connect to a chromium-based browser, and use it to run UI-based
-  //         // tests.
-  //         type: 'chromium',
-  //       },
-  //     },
-  //   },
-  // }
+  //stages: [
+  //  { duration: '10s', target: 10 }
+  //]
+  vus: 40,
+  duration: "60s",
+  insecureSkipTLSVerify: true,
+  setupTimeout: '120s',
 };
 
-// The function that defines VU logic.
-//
-// See https://grafana.com/docs/k6/latest/examples/get-started-with-k6/ to learn more
-// about authoring k6 scripts.
-//
-export default function() {
-  // http://localhost:3000/store/DpcbqSOX97g-kd3GlGSLa
-  const res = http.get('http://192.168.1.105:3001/queue/test?type=B&branch_no=BUUGFWjL0Ery_-o-T0mEH');
-  console.log(res.body)
+export default function () {
+  const query = {
+    transaction: true,
+    successRate: true,
+    channelUsages: true,
+    trendType: "hourly",
+    startTime: "2024-06-01T05:33:19.376Z",
+    endTime: "2024-07-20T05:33:19.376Z",
+  };
+
+  const url = new URL("https://192.168.1.100:9443/api/dashboard/summaryDashboard");
+
+  Object.keys(query).forEach((key) => {
+    url.searchParams.append(key, query[key])
+  })
+
+  const bearer =
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJwNEs2MDNFSGRtZFV1ZG5YUWczWkJvIiwicHVibGljSWQiOiJlbnRyb0NBQSIsInJvbGUiOiJDQUEiLCJleHAiOjE3MjE0NTE0MTAsInN5c0VtYWlsIjoiYWthcG9ybi5rQGVudHJvbmljYS5jby50aCIsImlhdCI6MTcyMTM2NTAxMH0.UnZg8DHbkJ9AHrihABhpc_qofr9kSGtyx2qFvgr7vqc";
+
+  const res = http.get(url.toString(), {
+    headers: {
+      "x-authorization": bearer,
+      "x-transaction": `${new Date().getTime()}`,
+    },
+    query: {
+      transaction: true,
+    },
+  });
   sleep(1);
 }
